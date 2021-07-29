7c25566d0b9049216f75afdf2199cbce0896506f
Vivianna Lieu <lieu.vivianna@gmail.com>
2021-07-29 13:22:23 -0500

    Initial Commit

---
+++ D4-mini-project-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  D4-mini-project-2
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
